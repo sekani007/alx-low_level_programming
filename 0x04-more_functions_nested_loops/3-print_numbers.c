b4cee90ca1b5b2c53a48e903481726e24ae0fa24
sekani007 <elvisokeke@gmail.com>
2023-08-17 15:59:06 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - A program that prints numbers from 0 -9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A program that checks for checks for a digit
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
+}
+
