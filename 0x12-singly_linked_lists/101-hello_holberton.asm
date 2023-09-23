d61dd103cdb49c3d6117af98dc4684f639409f6b
sekani007 <elvisokeke@gmail.com>
2023-09-22 17:46:14 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,9 @@
-section .data
-    hello db "Hello, Holberton",0   ; Null-terminated string to be printed
-
-section .text
-    global main
-
-extern printf
+	global   main
+	  extern    printf
 main:
-    push rbp
-    mov rdi, hello                 ; Format string for printf
-    call printf                    ; Call the printf function
-    add rsp, 8                     ; Adjust the stack pointer
-    pop rbp
-
-    ret                            ; Return from main
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
